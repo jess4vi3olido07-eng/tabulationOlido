b1ffece1c9a034e4df9b31f1490aacd73feda999
jess4vi3olido07-eng <jess4vi3olido07@gmail.com>
2025-10-20 17:53:43 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,33 @@
+function Geh(){
+let Tal1 = document.getElementById("Tal1").value;
+let Pro1= document.getElementById("Pro1").value;
+let Gown1=document.getElementById("Gown1").value;
+
+let ti = Tal1 * 0.30;
+let tii = Pro1 * 0.20;
+let tiii = Gown1 * 0.50;
+let sum = ti + tii + tiii;
+alert("Average: " + sum)
+}
+function Geh2(){
+let Ah = document.getElementById("Ah").value;
+let Ah2= document.getElementById("Ah2").value;
+let Ah3=document.getElementById("Ah3").value;
+
+let ma = Ah * 0.30;
+let maa = Ah2 * 0.20;
+let maaa = Ah3 * 0.50;
+let sum = ma + maa + maaa;
+alert("Average: " + sum)
+}
+function Geh3(){
+let hi = document.getElementById("hi").value;
+let hihi= document.getElementById("hihi").value;
+let hihihi=document.getElementById("hihihi").value;
+
+let ok = hi * 0.30;
+let okk = hihi * 0.20;
+let okkk = hihihi * 0.50;
+let sum = ok + okk + okkk;
+alert("Average: " + sum)
+}
